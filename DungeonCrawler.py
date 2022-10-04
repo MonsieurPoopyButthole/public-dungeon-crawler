c5b91864bd97ae552b40c58224c512c265666ad6
Rubin <rubinlemeti@gmail.com>
2022-10-04 16:24:09 +0200

    fixed the syntax a bit

--- DungeonCrawler.py
+++ DungeonCrawler.py
@@ -7,42 +7,43 @@ PLAYER = 'P'
 
 class Player:
     def __init__(self, gridSize):
-        self.xPos = random.randint(0,gridSize*2)
-        self.yPos = random.randint(0,gridSize)
-    
+        self.xPos = random.randint(0, gridSize * 2)
+        self.yPos = random.randint(0, gridSize)
+
     def xDisplacement(self, xVector):
         self.xPos = self.xPos + xVector
-    
+
     def yDisplacement(self, yVector):
         self.yPos = self.yPos + yVector
-        
+
     def position(self):
         print(str(self.xPos) + "=x  " + str(self.yPos) + "=y")
 
 
 gridSize = int(input("Enter grid size "))
-grid = [["."] * gridSize*2 for i in range(gridSize)]
+grid = [["."] * gridSize * 2 for i in range(gridSize)]
 playerCharacter = Player(gridSize)
 
-while(True):
+
+while True:
     chanceOfObstacle = float(input("Enter a percentage chance of position to be an obstacle "))
-    if(chanceOfObstacle <= 100 and chanceOfObstacle >= 0):
+    if 100 >= chanceOfObstacle >= 0:
         break
     print("Wrong value, re-enter it")
-    
 
-#fill grid
+
+# fill grid
 for i in range(gridSize):
-    for j in range(gridSize*2):
-        chance = random.randint(0, 100) 
-        if(chance < chanceOfObstacle):
+    for j in range(gridSize * 2):
+        chance = random.randint(0, 100)
+        if chance < chanceOfObstacle:
             grid[i][j] = OBSTACLE
 grid[playerCharacter.yPos][playerCharacter.xPos] = PLAYER
 
 
-#print grid
+# print grid
 for i in range(gridSize):
-    for j in range(gridSize*2):
+    for j in range(gridSize * 2):
         print(grid[i][j], end="")
     print()
 
